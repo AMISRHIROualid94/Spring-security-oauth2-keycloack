a1680f4a287d23b5f38bb4f18ee5d49650ac3ad9
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-02-25 03:13:04 +0100

    Keycloack oauth2 demo

--- oauth2-authorization-code-demo/src/main/java/com/oualid/springsecurityoauth2keycloack/demo/DemoApplication.java
+++ oauth2-authorization-code-demo/src/main/java/com/oualid/springsecurityoauth2keycloack/demo/DemoApplication.java
@@ -1,10 +1,11 @@
 package com.oualid.springsecurityoauth2keycloack.demo;
 
+//import org.springframework.security.oauth2.client.oidc.authentication.OidcAuthorizationCodeAuthenticationProvider
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class,args);
